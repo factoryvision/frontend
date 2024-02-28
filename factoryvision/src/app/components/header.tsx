83bf80609626204f3e8959439235841320ca7564
umin <108667311+mynameisumin@users.noreply.github.com>
2024-02-28 20:10:55 +0900

    컴포넌트 수정

--- factoryvision/src/app/components/header.tsx
+++ factoryvision/src/app/components/header.tsx
@@ -3,8 +3,7 @@ import Link from "next/link";
 const Header = () => {
   return (
     <div className="flex justify-between items-center p-4">
-      {/* <img src="/mainlogo.png" alt="visionfactory" className="w-70 h-10" /> */}
-
+      <div className="w-70 h-10" />
       <div className="flex items-center space-x-4  mr-10">
         <Link href="/user/login">
           <div className="text-white rounded-md px-4 py-2 text-sm bg-blue-500 w-32 text-center drop-shadow-lg">
