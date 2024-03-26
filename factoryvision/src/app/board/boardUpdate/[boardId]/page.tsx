51c13585c13457624312b69729952a5b1ba4981e
umin <108667311+mynameisumin@users.noreply.github.com>
2024-03-26 18:58:57 +0900

    board

--- factoryvision/src/app/board/boardUpdate/[boardId]/page.tsx
+++ factoryvision/src/app/board/boardUpdate/[boardId]/page.tsx
@@ -1,25 +1,22 @@
-// 게시물생성
+// 게시물수정
 "use client";
 
-// import { useEffect, useState } from "react";
 import Link from "next/link";
 import Sidebar from "@/app/components/sidebar";
 import Header from "@/app/components/header";
 import { ChangeEvent, useState, useEffect } from "react";
-import { useRouter } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
 
 const currentPage = 2;
 const totalPages = 5;
 
-export default function boardCreate() {
-  const [title, setTitle] = useState(""); // 제목 상태 추가
-  const [content, setContent] = useState(""); // 내용 상태 추가
+export default function boardUpdate() {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
   const router = useRouter();
-  // const params = useParams(); //2024-03-25 추가
-  // const boardId = params.boardId;
-  const [userId, setuserId] = useState("");
-  const [boardId, setboardId] = useState("");
-  // const userId = params.userId;
+  const params = useParams();
+  const id = params.boardId;
+  const userId = params.userId;
 
   const handleTitleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setTitle(e.target.value); // 제목 변경 핸들러
@@ -29,51 +26,53 @@ export default function boardCreate() {
     setContent(e.target.value); // 내용 변경 핸들러
   };
 
+  const handleSubmit = async () => {
+    try {
+      const response = await fetch(
+        `http://localhost:8080/factoryvision/board/${id}`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `${localStorage.getItem("access-token")}`,
+          },
+          body: JSON.stringify({
+            userId: userId,
+            title: title,
+            content: content,
+          }),
+        }
+      );
+      console.log(response);
+      if (response.ok) {
+        alert("수정이 완료되었습니다.");
+        router.push("/board/boardList");
+        router.refresh();
+      } else {
+        throw new Error("서버에서 올바른 응답을 받지 못했습니다.");
+      }
+    } catch (error) {
+      console.error("게시물 수정 중 오류 발생:", error);
+    }
+  };
   useEffect(() => {
-    // 사용자 정보 가져오는 API 호출   여기를 현재 사용자로
-    const fetchUserId = async () => {
-      console.log("userid", userId);
-      fetch(`http://localhost:8080/factoryvision/tokenInfo`, {
-        headers: {
-          Authorization: `${localStorage.getItem("access-token")}`,
-        },
-        method: "GET",
-      })
-        .then((response) => response.text())
-        .then((data) => {
-          userId;
-          console.log("아이디 확인", userId);
-        });
-    };
-    fetchUserId();
-  }, []);
-
-  const handleSubmit = () => {
-    fetch("http://localhost:8080/factoryvision/board", {
-      method: "post",
+    // 게시물 데이터 가져오기
+    fetch(`http://localhost:8080/factoryvision/board/${id}`, {
       headers: {
-        "Content-Type": "application/json",
         Authorization: `${localStorage.getItem("access-token")}`,
       },
-      body: JSON.stringify({
-        userId: 1,
-        title: title, //수정
-        content: content, //수정
-      }),
+      method: "GET",
     })
-      .then((response) => {
-        if (response.ok) {
-          console.log("게시글 작성 성공");
-          router.push("/board/boardList");
-        } else {
-          console.log(response);
-          console.error("게시글 작성 실패");
-        }
+      .then((Response) => Response.json())
+      .then((data) => {
+        console.log("boardDetail data", data);
+        setTitle(data.title);
+        setContent(data.content);
       })
-      .catch((error) => {
-        console.log("게시글 작성 오류", error);
-      });
-  };
+      .catch((error) =>
+        console.error("게시물 상세 정보를 가져오는 중 오류 발생:", error)
+      );
+  }, [id]);
 
   return (
     <div className="flex flex-col">
@@ -84,7 +83,7 @@ export default function boardCreate() {
           <div className="rid gap-x-8 gap-y-4 flex py-20 place-content-between ">
             {/*  */}
             <div className="text-blue-700 border-b-4 text-sm px-4 pb-4 w-30 border-blue-700 text-center">
-              게시글 작성
+              게시글 수정
             </div>
             {/*  */}
           </div>
@@ -95,17 +94,16 @@ export default function boardCreate() {
               className="w-full h-full bg-[#fff] text-[0.8125rem] rounded-[0.5rem] p-4 border border-blue-500"
               type="text"
               placeholder="제목을 입력해주세요."
-              value={title}
               onChange={handleTitleChange}
+              value={title}
             />
 
             <div>내용</div>
-            <input
+            <textarea
               className="w-full h-full bg-[#fff] text-[0.8125rem] rounded-[0.5rem] p-4 border border-blue-500"
               placeholder="내용을 입력해주세요."
-              type="text"
-              value={content}
               onChange={handleContentChange}
+              value={content}
             />
 
             <div className="pt-5">
@@ -113,11 +111,14 @@ export default function boardCreate() {
                 className="bg-blue-700 rounded-md mr-5 px-4 py-2 text-sm text-white"
                 onClick={handleSubmit}
               >
-                작성
+                수정
               </button>
 
-              <button className="bg-blue-700 rounded-md p-4 px-4 py-2 text-sm text-white">
-                <Link href="./boardList">취소</Link>
+              <button
+                className="bg-blue-700 rounded-md p-4 px-4 py-2 text-sm text-white"
+                onClick={() => router.push(`/board/boardDetail/${id}`)}
+              >
+                취소
               </button>
             </div>
           </div>
